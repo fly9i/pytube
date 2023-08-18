41f800ca85eddf85ec6d0afb7fb5ad641cbe894c
fzheng <fzheng@hillinsight.com>
2023-08-18 11:03:32 +0800

    update new version

--- pytube/captions.py
+++ pytube/captions.py
@@ -83,14 +83,14 @@ class Caption:
         """
         segments = []
         root = ElementTree.fromstring(xml_captions)
-        for i, child in enumerate(list(root)):
+        for i, child in enumerate(list(root[0])):
             text = child.text or ""
             caption = unescape(text.replace("\n", " ").replace("  ", " "),)
             try:
-                duration = float(child.attrib["dur"])
+                duration = float(child.attrib["d"]) / 1000.0
             except KeyError:
                 duration = 0.0
-            start = float(child.attrib["start"])
+            start = float(child.attrib["t"]) / 1000.0
             end = start + duration
             sequence_number = i + 1  # convert from 0-indexed to 1.
             line = "{seq}\n{start} --> {end}\n{text}\n".format(
